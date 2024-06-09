53fcb25afd511af503e49c733dc49bea6f3730c0
j65d <dbie9s@outlook.com>
2024-06-09 15:37:18 +0800

    Modify evidence.cpp

--- evidence.cpp
+++ evidence.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Inside material east stand speak stay.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
