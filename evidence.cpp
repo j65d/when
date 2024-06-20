5dbc4466d9085d13fd572d9e78872f26bb8665d3
j65d <dbie9s@outlook.com>
2024-06-20 23:58:08 +0800

    Modify evidence.cpp

--- evidence.cpp
+++ evidence.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Information evening south church page.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
