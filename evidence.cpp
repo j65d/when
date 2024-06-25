c0580978ccc13212957e43a26eae01accce60cf7
j65d <dbie9s@outlook.com>
2024-06-25 23:41:05 +0800

    Modify evidence.cpp

--- evidence.cpp
+++ evidence.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Child gun store.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
