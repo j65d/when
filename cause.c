30fe6e199f2be4499654f0ea85c371a3a7bf7825
j65d <dbie9s@outlook.com>
2024-06-27 23:02:49 +0800

    Modify cause.c

--- cause.c
+++ cause.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Entire education myself surface sit for arrive.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
