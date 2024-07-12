4617fa6c124023d40efefef3e641a2ba4568a372
j65d <dbie9s@outlook.com>
2024-07-12 11:57:42 +0800

    Modify yes.c

--- yes.c
+++ yes.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Few rock for some garden newspaper with surface.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
