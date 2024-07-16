fa22376cb998595f690803dd56037d7484c205a6
j65d <dbie9s@outlook.com>
2024-07-16 10:28:28 +0800

    Modify trouble.cpp

--- trouble.cpp
+++ trouble.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Move hand computer agree energy center travel.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
