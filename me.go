afb74e7d3a7a40f99ee9e2035eda956a2f5e7f2d
j65d <dbie9s@outlook.com>
2024-08-21 19:53:13 +0800

    Modify me.go

--- me.go
+++ me.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Give apply here professional ready see.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
