6d1acac14cbbaf037813979ec04153580b8c4b2c
j65d <dbie9s@outlook.com>
2024-09-04 13:47:27 +0800

    Modify total.cpp

--- total.cpp
+++ total.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Report generation wind simple.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
