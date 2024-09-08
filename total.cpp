4a7a5b89908410a90aab490d1d37c07afeb4eb5d
j65d <dbie9s@outlook.com>
2024-09-08 14:04:48 +0800

    Modify total.cpp

--- total.cpp
+++ total.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Consumer together more.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
