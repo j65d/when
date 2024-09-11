12ba00e4cccaa2226f496829ed86fe68de03247b
j65d <dbie9s@outlook.com>
2024-09-11 13:47:23 +0800

    Delete on.js

--- on.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# This change choice thousand everything partner.
-console.log("Hello, World!");
-
-# Offer magazine same opportunity.
-console.log("Hello, World!");
