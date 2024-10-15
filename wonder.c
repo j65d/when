6f48a1139e6af16a1267504d267e434e09a0017a
j65d <dbie9s@outlook.com>
2024-10-15 11:02:55 +0800

    Modify wonder.c

--- wonder.c
+++ wonder.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Author government new organization together.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
