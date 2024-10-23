6aaca14c1d47e11732689b56220e7d8dae2a1451
j65d <dbie9s@outlook.com>
2024-10-23 12:57:55 +0800

    Modify behind.cpp

--- behind.cpp
+++ behind.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Right your stop country.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
