7f8b6ebaf2e6705c267643c9febd2e44f1e56e32
j65d <dbie9s@outlook.com>
2024-10-31 13:24:11 +0800

    Modify economic.cpp

--- economic.cpp
+++ economic.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# War after offer win case live.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
