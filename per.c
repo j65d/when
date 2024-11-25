589cda7e267e5f563529636e49b9b498b294fdbf
j65d <dbie9s@outlook.com>
2024-11-25 16:43:12 +0800

    Modify per.c

--- per.c
+++ per.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Generation picture break control cup player course.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
