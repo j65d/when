ab99dbdeb594a1321b63422776223d1361e971b6
j65d <dbie9s@outlook.com>
2024-11-30 23:55:45 +0800

    Modify move.cpp

--- move.cpp
+++ move.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Space issue view between beyond friend particularly choice.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
