5d527735ecf8e525107481187f008740c9282a01
j65d <dbie9s@outlook.com>
2024-12-11 06:32:52 +0800

    Modify move.cpp

--- move.cpp
+++ move.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Senior just mind our region.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
