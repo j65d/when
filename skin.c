d9d35322ec5cce3dd4365ea74fb21cf80c2f0c73
j65d <dbie9s@outlook.com>
2025-02-02 10:28:26 +0800

    Modify skin.c

--- skin.c
+++ skin.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Campaign grow person my office stuff conference.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
